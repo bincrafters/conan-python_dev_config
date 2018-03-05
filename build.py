658a60bc7910bff22f1bd07782d0ff8e5c5fbbb0
Rene Rivera <grafikrobot@gmail.com>
2018-03-05 16:42:09 -0600

    Stick to options and almost plain Travis.

--- build.py
+++ build.py
@@ -6,4 +6,6 @@ from bincrafters import build_template_header_only
 
 if __name__ == "__main__":
     builder = build_template_header_only.get_builder()
+    options = dict(map(lambda o: o.split('='), os.environ['CONAN_OPTIONS'].split(';')))
+    builder.add(options=options)
     builder.run()
