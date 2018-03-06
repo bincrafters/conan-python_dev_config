5b3b9fdcd18efb9ebb0ebfa6d2eaa888b15b3736
Rene Rivera <grafikrobot@gmail.com>
2018-03-05 22:55:10 -0600

    Clear out default builds. And need to set compiler on macos for conan.

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ import os
 
 if __name__ == "__main__":
     builder = build_template_header_only.get_builder()
+    build.builds = []
     options = dict(map(lambda o: o.split('='), os.getenv('CONAN_OPTIONS').split(';'))) if os.getenv('CONAN_OPTIONS', None) else None
     builder.add(options=options)
     builder.run()
