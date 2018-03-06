2b485a76dc621273f9333e53cf758ca30359b7f0
Rene Rivera <grafikrobot@gmail.com>
2018-03-05 22:57:31 -0600

    Fix syntax error build => builder.

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ import os
 
 if __name__ == "__main__":
     builder = build_template_header_only.get_builder()
-    build.builds = []
+    builder.builds = []
     options = dict(map(lambda o: o.split('='), os.getenv('CONAN_OPTIONS').split(';'))) if os.getenv('CONAN_OPTIONS', None) else None
     builder.add(options=options)
     builder.run()
