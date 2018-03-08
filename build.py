66b40bcebcefe3e6c7b8df3da4baec16b6752655
Rene Rivera <grafikrobot@gmail.com>
2018-03-08 16:00:30 -0600

    Fix typo.

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     builder = build_template_header_only.get_builder()
     builder.builds = []
     options = dict(map(lambda o: o.split('='), os.getenv('CONAN_OPTIONS').split(';'))) if os.getenv('CONAN_OPTIONS', None) else None
-    archs = os.getenv('CONAN_ARCHS','').splt(',')
+    archs = os.getenv('CONAN_ARCHS','').split(',')
     if archs and len(archs) > 0:
         for arch in archs:
             builder.add({'arch':arch},options=options)
