3ff114eeec66d99da8d5767291b643498e83c4c8
Rene Rivera <grafikrobot@gmail.com>
2018-03-08 15:09:08 -0600

    Parse CONAN_ARCHS as we are doing a partial add_common_builds.

--- build.py
+++ build.py
@@ -8,5 +8,10 @@ if __name__ == "__main__":
     builder = build_template_header_only.get_builder()
     builder.builds = []
     options = dict(map(lambda o: o.split('='), os.getenv('CONAN_OPTIONS').split(';'))) if os.getenv('CONAN_OPTIONS', None) else None
-    builder.add(options=options)
+    archs = os.getenv('CONAN_ARCHS','').splt(',')
+    if archs and len(archs) > 0:
+        for arch in archs:
+            builder.add({'arch':arch},options=options)
+    else:
+        builder.add(options=options)
     builder.run()
