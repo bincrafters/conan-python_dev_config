939632fffd2e887cee71f9f944578a63f8cb2a1b
Jerry W <jerrywiltse@gmail.com>
2018-08-16 04:13:05 -0400

    add arch check condition in build.py

--- build.py
+++ build.py
@@ -8,20 +8,25 @@ if __name__ == "__main__":
 
     builder = build_template_header_only.get_builder()
     
+    arch = os.getenv('conan_archs', None)
+    
     updated_settings={
-        'arch':os.getenv('conan_archs')
+        'arch': arch,
     }
     
     updated_options={
         "python_dev_config:python" : os.getenv("conan_python_path", "python"),
     }
            
-
     updated_builds = []
 
     for settings, options, env_vars, build_requires, reference in builder.items:
-         updated_builds.append([updated_settings, updated_options, env_vars, build_requires])
-    
+        if arch is not None:
+             updated_builds.append([updated_settings, updated_options, env_vars, build_requires])
+        else:
+             updated_builds.append([settings, updated_options, env_vars, build_requires])
+        
+        
     builder.builds = updated_builds    
     
     builder.run()
