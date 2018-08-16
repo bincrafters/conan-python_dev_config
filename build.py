b8c1356036e75690c0ab39e246d0025d223884e4
Jerry W <jerrywiltse@gmail.com>
2018-08-16 01:47:52 -0400

    use more standard method like we did with boost_python for passing options to builder

--- build.py
+++ build.py
@@ -5,13 +5,23 @@ from bincrafters import build_template_header_only
 import os
 
 if __name__ == "__main__":
+
     builder = build_template_header_only.get_builder()
-    builder.builds = []
-    options = dict(map(lambda o: o.split('='), os.getenv('CONAN_OPTIONS').split(';'))) if os.getenv('CONAN_OPTIONS', None) else None
-    if os.getenv('CONAN_ARCHS', None):
-        archs = os.getenv('CONAN_ARCHS').split(',')
-        for arch in archs:
-            builder.add({'arch':arch},options=options)
-    else:
-        builder.add(options=options)
+    
+    updated_settings={
+        'arch':os.getenv('conan_archs')
+    }
+    
+    updated_options={
+        "python_dev_config:python" : os.getenv("conan_python_path", "python"),
+    }
+           
+
+    updated_builds = []
+
+    for settings, options, env_vars, build_requires, reference in builder.items:
+         updated_builds.append([updated_settings, updated_options, env_vars, build_requires])
+    
+    builder.builds = updated_builds    
+    
     builder.run()
